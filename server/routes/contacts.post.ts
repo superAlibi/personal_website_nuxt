c8b0082b69559a74f7921d0e85ffa5c4ec1b794e
xjm <alibi_jia@foxmail.com>
2024-11-06 22:28:45 +0800

    fix:服务端重定向错误

--- server/routes/contacts.post.ts
+++ server/routes/contacts.post.ts
@@ -1,5 +1,4 @@
 import dayjs from "dayjs";
-import { navigateTo } from "nuxt/app";
 
 export default defineEventHandler(async (event) => {
 
@@ -8,7 +7,7 @@ export default defineEventHandler(async (event) => {
   const result = (await GetCredit(st)).at(0);
   if (!result) {
     console.warn("留言提交:含有st但没有找到对应的公司信息:" + st);
-    return navigateTo('/')
+    return sendRedirect(event, "/", 302);
   }
   const driveId = getCookie(event, 'di');
   if (!driveId) {
@@ -16,14 +15,14 @@ export default defineEventHandler(async (event) => {
       `留言提交:含有公司信息,但是无法拿到cookie的driveId:${result.corporateName}`,
     );
 
-    return navigateTo('/')
+    return sendRedirect(event, "/", 302);
   }
   const dindex = result.drives.findIndex((i) => i.driveId === driveId);
   if (dindex < 0) {
     console.warn(
       `留言提交:含有公司信息,但是无法找到设备信息:${driveId}`,
     );
-    return new Response(null, { status: 302, headers: { "location": "/" } });
+    return sendRedirect(event, "/", 302);
   }
   const driveInfo = result.drives.at(dindex)!;
   if (!Array.isArray(driveInfo.messages)) {
